a34b519ead5f63fd9532ef01cd42c424fe3129c2
20161104587 <a20161104587@chu1e1adeiMac.local>
2018-09-12 11:27:24 +0800

    1++
    1add100

---
+++ 2.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var sum=0
+let x=4
+let y=5
+print(x+y)
+
+for i in 1...100{
+    sum+=i
+    print(i)
+}
+print(sum)
